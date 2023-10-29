492fc904ed11a7c1561871f89555b2365452cad2
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2023-10-29 06:58:13 -0700

    Create page.js

---
+++ app/blog/page.js
@@ -0,0 +1,187 @@
+export default function () {
+    return(
+        <>
+        
+        <div  style={{width:"100%"}}>
+        
+<div  className='columns-4 top-0 right-0 grid grid-cols-10 gap-4'  >
+  
+  <h3 style={{color:"#F9FAFB", fontSize:"27px",marginLeft:"100px", paddingTop:"25px", fontFamily:'Open',width:"200px"}}>Anime Alley</h3>
+  <h2 className='right-0 transform transition duration-300 hover:scale-105' style={{color:"#F9FAFB", fontSize:"18px",marginLeft:"180px", paddingTop:"33px", fontFamily:'Openr',width:"100px"}}>YouTube</h2>
+  <h2 className='right-0 transform transition duration-300 hover:scale-105' style={{color:"#F9FAFB", fontSize:"18px",marginLeft:"120px", paddingTop:"33px", fontFamily:'Openr',width:"100px"}}>Games</h2>
+  <h2 className='right-0 transform transition duration-300 hover:scale-105' style={{color:"#F9FAFB", fontSize:"18px",marginLeft:"50px", paddingTop:"33px", fontFamily:'Openr',width:"100px"}}>Space</h2>
+  <h2 className='right-0 transform transition duration-300 hover:scale-105' style={{color:"#F9FAFB", fontSize:"18px",marginLeft:"-25px", paddingTop:"33px", fontFamily:'Openr',width:"100px"}}>Blogs</h2>
+  <div className="flex items-center">
+    <div className="flex space-x-1 absolute top-7 right-5">
+        <input
+            type="text"
+            className="block w-full px-4 py-2 text-black bg-gray border rounded-full focus:border-gold-400 focus:ring-gold-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            placeholder="Search..."
+        />
+        {/* <button className="px-4 text-white rounded-full " style={{backgroundColor:"#403f3f",marginLeft:"12px"}}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+            </svg>
+        </button> */}
+    </div>
+</div>
+{/* <img className='absolute top-0 right-0' style={{width:"42px",paddingTop:"28px",marginRight:"1.5%"}} src="https://img.icons8.com/windows/64/FFFFFF/user-male-circle.png" alt="" /> */}
+
+
+ </div> <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-500"></hr>
+ 
+<div style={{marginLeft:"15%",marginBottom:"20px"}} class="inline-flex rounded-md shadow-sm" role="group">
+<button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+Discover
+</button>
+<button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+Browser
+</button>
+<button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+NEWS
+</button>
+</div>
+
+ {/* <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Light</button> */}
+ <center>
+<div className='columns-2' style={{width:"70%"}}>
+
+<div className='row' style={{width:"100%"}}>
+<img className='max-w-full h-auto' style={{borderRadius:"15px"}} draggable={false} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p align="left"  style={{fontFamily:'Open',color:"#F9FAFB", fontSize:"12px",marginTop:"5px"}}>1D Ago</p>
+<p align="left"  style={{fontFamily:'Open',color:"#F9FAFB", fontSize:"20px"}}>A Small Sized Image Main Topic Example</p>
+<p align="left"  style={{fontFamily:'Openr',color:"#F9FAFB", fontSize:"15px",marginTop:"5px"}}>Like running and gunning? Want to carry a big stick? What’s the difference between yellow and red magic—and why should you care? We’ve got the answers.</p>
+<p align="left" style={{marginTop:"10px"}}>Read More</p>
+
+</div>
+<div className='row' style={{width:"100%"}}>
+<img style={{borderRadius:"15px"}} draggable={false} src="https://cdn.vox-cdn.com/thumbor/0y-hyENf_E1C61JydjRSpTjS0TA=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/16346203/064_Dillon_Danis_x_Max_Humphrey.jpg" alt="" />
+<p align="left"  style={{fontFamily:'Open',color:"#F9FAFB", fontSize:"12px",marginTop:"5px"}}>1D Ago</p>
+<p align="left"  style={{fontFamily:'Open',color:"#F9FAFB", fontSize:"20px"}}>A Off Size / Bigger than usual size image  [squre] image example</p>
+<p align="left"  style={{fontFamily:'Openr',color:"#F9FAFB", fontSize:"15px",marginTop:"5px"}}>Like running and gunning? Want to carry a big stick? What’s the difference between yellow and red magic—and why should you care? We’ve got the answers.</p>
+<p align="left" style={{marginTop:"10px"}}>Read More</p>
+
+</div>
+</div> 
+
+<hr style={{marginTop:"40px"}} class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-500"></hr>
+
+
+<br />
+<div className='columns-4' style={{width:"70%", marginTop:"10px"}}>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+
+</div>
+<hr style={{width:"70%"}} class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-500"></hr>
+<div className='columns-4' style={{width:"70%"}}>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+
+</div>
+<hr style={{width:"70%"}} class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-500"></hr>
+<div className='columns-4' style={{width:"70%"}}>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+<div> 
+<img style={{borderRadius:"20px"}} src="https://cdn2.unrealengine.com/how-ubisoft-turned-an-urban-mirage-into-a-real-city-in-assassin-s-creed-mirage-1920x1080-a32aaaf75c5a.jpg" alt="" />
+<p  className='' style={{fontFamily:"open",fontSize:"12px", marginTop:"5px"}} align="left">1D Ago</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"18px", marginTop:"3px"}}>Sunless Sea grips you with the fear of the deep</p>
+<p align="left" style={{fontFamily:"openr", fontSize:"16px",marginTop:"5px"}}>Read More</p>
+</div>
+
+</div>
+{/* <hr style={{width:"70%"}} class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-500"></hr> */}
+<br />
+<br />
+<div style={{width:"70%" }} className='place-items-center'>
+<nav aria-label="Pagination" class="flex items-center text-gray-600">   
+<a href="#" class="p-2 mr-4 rounded hover:bg-gray-100">      
+<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">      
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />      </svg>    </a>  
+<a href="#" class="px-4 py-2 rounded hover:bg-gray-100"> 1 </a>    
+<a href="#" class="px-4 py-2 rounded bg-gray-200 text-gray-900 font-medium hover:bg-gray-100"> 2 </a>
+ <a href="#" class="px-4 py-2 rounded hover:bg-gray-100"> 3 </a>    <a href="#" class="px-4 py-2 rounded hover:bg-gray-100"> ... </a>  
+   <a href="#" class="px-4 py-2 rounded hover:bg-gray-100"> 9 </a>    <a href="#" class="p-2 ml-4 rounded hover:bg-gray-100"> 
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">    
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />      </svg>    </a> 
+             </nav>
+</div><br />
+</center>
+</div>
+
+        
+        </>
+    )
+}
